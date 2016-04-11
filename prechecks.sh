d9fe232401268e05a91a7d8a6eed6f24e5849c64
Popa Bogdan Constantin <bogdan.popa@reea.net>
2016-04-11 11:42:52 +0300

    asd

--- prechecks.sh
+++ prechecks.sh
@@ -4,6 +4,16 @@
 echo 'Starting Prechecks';
 echo '------------------';
 
+echo "Removing GIT folder if exists";
+if test -e $GitFolder;
+	then
+		echo "WE HAVE FOUND THE GIT FOLDER";
+		cd $GitFolder;
+		ls -a;
+	else
+		echo "Continuing , git folder not found yet.";
+fi
+
 echo "Database Should be Cleaned Here Using The New Script.";
 
 echo 'Prechecks finished.';
