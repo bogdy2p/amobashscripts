da0c54d2885578f585c22823f090ff1cf87bf814
Popa Bogdan Constantin <bogdan.popa@reea.net>
2016-04-11 11:49:14 +0300

    updates

--- symlinks.sh
+++ symlinks.sh
@@ -2,6 +2,8 @@
 . ./bashconfiguration.sh;
 
 echo 'Symlinking cleanup_logs.php';
+echo "$GitFolder/scripts/cleanup_logs.php ------- $HtdocsFolder/shell/cleanup_logs.php";
 ln -s $GitFolder/scripts/cleanup_logs.php $HtdocsFolder/shell/cleanup_logs.php;
 echo 'Symlinking cleanup_logs.php';
+echo "$GitFolder/scripts/core_url_backup.php -------- $HtdocsFolder/shell/core_url_backup.php";
 ln -s $GitFolder/scripts/core_url_backup.php $HtdocsFolder/shell/core_url_backup.php
