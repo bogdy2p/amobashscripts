7147f632c47fe05f7ff19c2021465d9c5138c10f
Popa Bogdan <bogdy2p@gmail.com>
2016-04-30 12:30:52 +0300

    Updated baseurl

--- bashconfiguration.sh
+++ bashconfiguration.sh
@@ -8,13 +8,13 @@ HtdocsFolder=$GitFolder'/htdocs';
 ScriptsFolder=$GitFolder'/scripts';
 DatabaseName='simulate';
 DatabasePwd='romania';
-DatabaseDump='/home/pbc/Desktop/09-march-2016-fulldb-fulltables.sql';
+DatabaseDump='/home/pbc/Desktop/truncated.sql';
 SymlinkableMediaFolder='/home/pbc/Desktop/amoranach_media/media';
 AdminUsername='admin';
 AdminPassword='admin123';
 AdminEmail='bogdan.popa@reea.net';
 SettingsCsvFile='/var/www/html/settings.csv';
-LiveUrl='http://simulate.com/';
+LiveUrl='https://dev.simulate.com/';
 GitAdress='http://git.reea.net/reea/amorana.git';
 ############################################################################
 ############################################################################
