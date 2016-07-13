1171f701a06920fd53a2eac36d2b4433d19aa427
Popa Bogdan <bogdy2p@gmail.com>
2016-07-13 14:42:02 +0300

    Updated

--- bashconfiguration.sh
+++ bashconfiguration.sh
@@ -13,7 +13,7 @@ SymlinkableMediaFolder='/home/pbc/Desktop/amoranach_media/media';
 AdminUsername='admin';
 AdminPassword='admin123';
 AdminEmail='bogdan.popa@reea.net';
-SettingsCsvFile='/var/www/html/settings.csv';
+SettingsCsvFile='/var/www/html/settings_amo.csv';
 LiveUrl='https://dev.simulate.com/';
 GitAdress='-b change/PhoenixVarnishToTurpentine http://git.reea.net/reea/amorana.git';
 ############################################################################
